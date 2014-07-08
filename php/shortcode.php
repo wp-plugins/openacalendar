93a46f9a85eacab53824105f17da15b98998018c
jarofgreen <jarofgreen@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-07-08 13:10:27 +0000

    Realease 2.0.5 of OpenACalendar
    git-svn-id: https://plugins.svn.wordpress.org/openacalendar/tags/2.0.5@944928 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- php/shortcode.php
+++ php/shortcode.php
@@ -16,8 +16,13 @@ function OpenACalendar_shortcode_events( $atts, $content="" ) {
 		'descriptionmaxlength'=>300,
 		'usesummarydisplay'=>true,
 		'startformat'=>'D jS M g:ia',
+		'endformatsameday'=>'',
+		'endformat'=>'',
+		'startandenddivider' => ' to ',
 		'eventcount'=>20,
+		'url'=>'site',
 	), $atts );
+	$attributes['usesummarydisplay'] = OpenACalendar_shortcode_attribute_to_boolean($attributes['usesummarydisplay']);
 	
 	require_once dirname(__FILE__).DIRECTORY_SEPARATOR."database.php";
 
@@ -26,15 +31,29 @@ function OpenACalendar_shortcode_events( $atts, $content="" ) {
 	if ($attributes['poolid']) {
 	
 		foreach(OpenACalendar_db_getNextEventsForPool($attributes['poolid'], $attributes['eventcount']) as $event) {
+			$url = $attributes['url'] == 'url' ? $event->getUrl() : $event->getSiteurl();
 			$html .= '<div class="OpenACalendarWidgetListEventsEvent">';
-			$html .= '<div class="OpenACalendarWidgetListEventsDate">'.$event->getStartAtAsString($event->getTimezone(), $attributes['startformat']).'</div>';
-			$html .= '<div class="OpenACalendarWidgetListEventsSummary"><a href="'.htmlspecialchars($event->getSiteurl()).'">'.
+			$end = null;
+			if ($attributes['endformatsameday'] || $attributes['endformat']) {
+				$format = $attributes['endformat'];
+				if ($event->isStartAndEndOnSameDay($event->getTimezone()) && $attributes['endformatsameday']) {
+					$format = $attributes['endformatsameday'];
+				}
+				$end = $event->getStartAtAsString($event->getTimezone(), $format);
+			}
+			if ($end) {
+				$html .= '<div class="OpenACalendarWidgetListEventsDate">'.$event->getStartAtAsString($event->getTimezone(), $attributes['startformat']).
+					$attributes['startandenddivider'].$end.'</div>';
+			} else {
+				$html .= '<div class="OpenACalendarWidgetListEventsDate">'.$event->getStartAtAsString($event->getTimezone(), $attributes['startformat']).'</div>';
+			}
+			$html .= '<div class="OpenACalendarWidgetListEventsSummary"><a href="'.htmlspecialchars($url).'">'.
 				htmlspecialchars($attributes['usesummarydisplay'] ? $event->getSummaryDisplay() : $event->getSummary()).
 				'</a></div>';	
 			if ($attributes['descriptionmaxlength'] > 0) {
 				$html .= '<div class="OpenACalendarWidgetListEventsDescription">'.htmlspecialchars($event->getDescriptionTruncated($attributes['descriptionmaxlength'])).'</div>';
 			}
-			$html .= '<a class="OpenACalendarWidgetListEventsMoreLink" href="' . $event->getSiteurl() . '">More Info</a>';
+			$html .= '<a class="OpenACalendarWidgetListEventsMoreLink" href="' . htmlspecialchars($url). '">More Info</a>';
 			$html .= '</div>';
 		}
 		
@@ -46,5 +65,16 @@ function OpenACalendar_shortcode_events( $atts, $content="" ) {
 	
 
 	return $html;
+}
+
+function OpenACalendar_shortcode_attribute_to_boolean($in) {
+	if ($in === true) {
+		return true;
+	} else if ($in === false) {
+		return false;
+	} else {
+		$in = strtolower(trim($in));
+		return substr($in,0,2) == 'on' || in_array( substr($in,0,1), array('1','t','y'));
+	}
 }
 
