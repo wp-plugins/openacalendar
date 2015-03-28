fea64dd1e6db1819e210a2c8bc74e9841a00a67a
jarofgreen <jarofgreen@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-28 13:41:50 +0000

    release 2.0.12
    git-svn-id: https://plugins.svn.wordpress.org/openacalendar/tags/2.0.12@1122708 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- php/admin.php
+++ php/admin.php
@@ -177,7 +177,7 @@ function OpenACalendar_admin_menu() {
 
 
 			print '<h4>usesummarydisplay</h4>';
-			print '<div>Does display title include group or not? Boolean. "true" or "false".</div>';
+			print '<div>Does display title include group or not? "yes" or "no".</div>';
 			print '<input type="text" name="attribute_usesummarydisplay" value="'.htmlspecialchars($attributes['usesummarydisplay']).'">';
 
 			print '<h4>startformat</h4>';
@@ -207,6 +207,10 @@ function OpenACalendar_admin_menu() {
 			print '<h4>image</h4>';
 			print '<div>Whether to show image. "false", "full" for full size, "normal" for normal size or a pixel dimension for a custom size.</div>';
 			print '<input type="text" name="attribute_image" value="'.htmlspecialchars($attributes['image']).'">';
+
+			print '<h4>eventshowmorelink</h4>';
+			print '<div>Whether to show "more" link under each event. "yes" or "no".</div>';
+			print '<input type="text" name="attribute_eventshowmorelink" value="'.htmlspecialchars($attributes['eventshowmorelink']).'">';
 
 
 			print '<div style="padding-top: 40px;"><input type="submit" value="Update Shortcode with your options"></div>';
